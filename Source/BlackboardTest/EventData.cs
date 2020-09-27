09bb6309e5f525797f512bb086f7d17c2701d59b
pvserg <32489978+pvserg@users.noreply.github.com>
2020-09-28 00:45:58 +0300

    Add files via upload

---
+++ Source/BlackboardTest/EventData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackboardTest
+{
+    public class EventData
+    {
+        public EventData(string eventName, long eventCount, DateTime eventDate)
+        {
+            EventName = eventName;
+            EventCount = eventCount;
+            EventDate = eventDate;
+        }
+
+        public string EventName { get; private set; }
+
+        public long EventCount { get; private set; }
+
+        public DateTime EventDate { get; private set; }
+
+    }
+}
